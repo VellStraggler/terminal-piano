1c810bf68acd74be19527fb75e2a301900b2e77c
VellStraggler <david@incwells.com>
2025-01-21 16:58:39 -0700

    using wav files instead of midi

--- piano.py
+++ piano.py
@@ -1,6 +1,7 @@
-
-import pygame.midi
 import keyboard
+from playsound import playsound # version 1.2.2
+from os.path import dirname
+import concurrent.futures
 
 ERASE_CODE = "\033[2J"
 RETURN_CODE = "\033[H"
@@ -12,7 +13,7 @@ _____________________________________
 |██|██|██|██|██|██|██|██|██|██|██|██|
 |██|██|██|██|██|██|██|██|██|██|██|██|'''
 #/\^/\^/\ /\^/\^/\^/\ /\^/\^/\ /\^
-keyboard_list = list("a w s e d f t g y h u j k o l p ; ' ] enter".split())
+keyboard_list = list("a w s e d f t g y h u j k o l p ; ' ]".split())
 black_keys = [1, 3, 6, 8, 10]
 note_points = [0,2,3,5]
 
@@ -21,20 +22,23 @@ reset_code = "\033[0m"
 
 arrow_list = ["^","/\\"]
 blank_list = [" ","  "]
+
+FILE_PREFIX = dirname(__file__) + "/notes/"
     
 print(ERASE_CODE)
-def draw_played_notes(midi_output: pygame.midi.Output, keys_held: set):
+def draw_played_notes(keys_held: set, pool):
     msg = " "
-    for i in range(0,20):
+    for i in range(0,19):
         target_key = keyboard_list[i]
-        pitch = i + 48
+        pitch = i
         if keyboard.is_pressed(target_key):
             if target_key not in keys_held:
-                play_note(midi_output, pitch, 70)
+
+                pool.submit(playsound, (FILE_PREFIX + str(pitch) + ".wav"))
+
                 keys_held.add(target_key)
             draw_list = arrow_list
         else:
-            play_note(midi_output, pitch, 0)
             if target_key in keys_held:
                 keys_held.remove(target_key)
             draw_list = blank_list
@@ -47,35 +51,22 @@ def draw_played_notes(midi_output: pygame.midi.Output, keys_held: set):
                 msg += " "
     print(msg)
 
-def setup_midi_output():
-    pygame.midi.init()
-    return pygame.midi.Output(pygame.midi.get_default_output_id())
-
-def play_note(midi_output, pitch, velocity):
-    if velocity > 0:
-        midi_output.note_on(pitch, velocity)
-    else:
-        midi_output.note_off(pitch)
-
-
 def main():
-
-    midi_output = setup_midi_output()
 
     run_program = True
     keys_pressed = set()
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=32)
 
     while run_program:
 
         print(DISPLAY)
-        draw_played_notes(midi_output, keys_pressed)
+        draw_played_notes(keys_pressed, pool)
 
         # COMMANDS
         if keyboard.is_pressed("q"):
             run_program = False
-
-    midi_output.close()
-    pygame.midi.quit()
+    
+    pool.shutdown(wait=False)
 
 if __name__ == "__main__":
     main()

d7349702809dd454dd7b3f19388196d5606d2eb2
VellStraggler <david@incwells.com>
2025-01-21 23:23:01 -0700

    lower-quality audio

--- piano.py
+++ piano.py
@@ -2,6 +2,8 @@ import keyboard
 from playsound import playsound # version 1.2.2
 from os.path import dirname
 import concurrent.futures
+import soundfile as sf
+import sounddevice as sd
 
 ERASE_CODE = "\033[2J"
 RETURN_CODE = "\033[H"
@@ -13,12 +15,8 @@ _____________________________________
 |██|██|██|██|██|██|██|██|██|██|██|██|
 |██|██|██|██|██|██|██|██|██|██|██|██|'''
 #/\^/\^/\ /\^/\^/\^/\ /\^/\^/\ /\^
-keyboard_list = list("a w s e d f t g y h u j k o l p ; ' ]".split())
+keyboard_list = list("a w s e d f t g y h u j k o l p ; ' ] enter".split())
 black_keys = [1, 3, 6, 8, 10]
-note_points = [0,2,3,5]
-
-red_code = "\033[31m"
-reset_code = "\033[0m"
 
 arrow_list = ["^","/\\"]
 blank_list = [" ","  "]
@@ -28,13 +26,13 @@ FILE_PREFIX = dirname(__file__) + "/notes/"
 print(ERASE_CODE)
 def draw_played_notes(keys_held: set, pool):
     msg = " "
-    for i in range(0,19):
+    for i in range(0,20):
         target_key = keyboard_list[i]
         pitch = i
         if keyboard.is_pressed(target_key):
             if target_key not in keys_held:
 
-                pool.submit(playsound, (FILE_PREFIX + str(pitch) + ".wav"))
+                pool.submit(playsound, (FILE_PREFIX + str(pitch) + ".1.wav"))
 
                 keys_held.add(target_key)
             draw_list = arrow_list
@@ -55,7 +53,7 @@ def main():
 
     run_program = True
     keys_pressed = set()
-    pool = concurrent.futures.ThreadPoolExecutor(max_workers=32)
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=16)
 
     while run_program:
 
